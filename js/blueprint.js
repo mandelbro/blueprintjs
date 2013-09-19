5be9d40582ef1b3047ee76759e06141f55321f36
Chris Montes <chrismontes@about.me>
2013-09-19 00:09:26 -0700

    Revert "Merge branch 'master' of github.com:mandelbro/blueprintjs"
    This reverts commit fb03f0a86f1227917d97bac64ee0780c93390959, reversing
    changes made to 69111409c60bb13cc6b4dafccf23bb1f7d5cfe9b.

--- js/blueprint.js
+++ js/blueprint.js
@@ -1,102 +1,134 @@
-var Blueprint = (function( $, window, document, undefined )  {
 
-  var Blueprint = function( ) {
-      // The blueprint prototype will decorate the extending view controller with it's functions
-      return Blueprint.fn.call( this );
-    };
+var Blueprint = (function( $ )  {
 
-  // Blueprint internal functions
-  Blueprint.fn = function( ) {
-    return {
-      init : function( initElement ) {
-        // don't proceed without an init elemnet
-        if(!initElement || !initElement.length) return;
+	var Blueprint = function( module, initElement ) {
+			// The Blueprint object is actually just the init constructor 'enhanced'
+			return new Blueprint.fn.init( module, initElement );
+		},
+		controllers = {};
 
-        // define the elements object
-        this.elements = {
-          main : initElement // important we remove blueprint-js here otherwise we'll get an infinite loop
-        };
+	// we'll just borrow $.extend from jQuery
+	Blueprint.extend = jQuery.fn.extend;
+	Blueprint.fn = Blueprint.prototype = {
+		constructor : Blueprint,
+		init : function( module, initElement ) {
+			// extend the view controller module
+			$.extend(this, module);
+			// run build
+			this.build(initElement);
+			// run the init hook
+			if(typeof this.hookInit == 'function') {
+				this.hookInit();
+			}
+			return this;
+		}
+	};
 
-        // define the template object
-        this.templates = {};
+	Blueprint.fn.init.prototype = Blueprint;
 
-        // define the model object
-        var jsonElement = initElement.find('.json');
-        this.model = {
-          json : jsonElement.length ? jsonElement.remove() : false,
-          data : jsonElement.length ? $.parseJSON(jsonElement.text()) : {}
-        };
-        // run build
-        if( typeof this.build == 'function') this.build();
-        // return the object
-        return this;
-      },
+	Blueprint.extend({
 
-      /**
-       * Blueprint.theme
-       * Theme handling method
-       *
-       * @param template
-       *    The template to insert from the .templates object
-       * @param variables
-       *    Map of variables to make available to the template
-       * @param options
-       *    An object of optional parameters
-       *    - offsetElement: the element the theme will be inserted into or around based on the insertMethod, if not specified the theme output will be returned
-       *    - insertMethod: append | prepend | insertAfter | insertBefore
-       *    - preprocess: a function to run before the theme function is run, receives variables as an argument
-       *    - postprocess: a function to run after the theme function is run but before it is inserted into the DOM, receives the theme output as an argument
-       *    - afterInsert: a function to run after the element is inserted onto the DOM, receives the offset element as an argument
-       */
-      theme : function( template, variables, options ) {
-        var buffer;
+		/**
+		 * Blueprint.build
+		 * Builds the resource list view controller output elements for the first
+		 * time.
+		 * Declares the this.element map that will hold specific elements
+		 * Declares the this.templates map that allows you to use the theme system
+		 *
+		 * @param initElement
+		 * 		The init element for the view controller
+		 */
+		build : function(initElement) {
+			// define the elements object
+			this.elements = {
+				main : initElement.removeClass('blueprint-js') // important we remove blueprint-js here otherwise we'll get an infinite loop
+			};
+			// provide a hook to register more elements
+			if(typeof this.hookElements == 'function') {
+				$.extend(this.elements, this.hookElements(initElement)); // this method should return an object of jQuery elements
+			}
 
-        // if the specfied template isn't available bail
-        if(typeof this.templates[template] != 'function') return;
+			// define the template object
+			this.templates = {};
 
-        // normalize inputs
-        options = $.extend(options, {
-          'insertMethod'  : 'append',
-          'offsetElement' : {},
-          'preprocess'    : null,
-          'postprocess'   : null,
-          'afterInsert'   : null
-        });
+			// provide a hook to add template functions to the template object
+			if(typeof this.hookTheme == 'function') {
+				this.templates = this.hookTheme(); // this method should return an object of template functions
+			}
 
-        variables = !variables ? {} : variables;
+			// define the model object
+			var jsonElement = initElement.find('.json:first');
+			this.model = {
+				json : jsonElement.length ? jsonElement.remove() : false,
+				data : jsonElement.length ? $.parseJSON(jsonElement.text()) : {}
+			};
 
-        // run the preprocess method if provided
-        if(typeof options.preprocess == 'function') {
-          options.preprocess(variables);
+			// provide a hook for adding model functions to the model object
+			if(typeof this.hookModel == 'function') {
+				$.extend(this.model, this.hookModel()); // this will give you a place to implement model functions and properties
+			}
+		},
+
+		/**
+		 * Blueprint.theme
+		 * Theme handling method
+		 *
+		 * @param template
+		 * 		The template to insert from the .templates object
+		 * @param variables
+		 *		Map of variables to make available to the template
+		 * @param element
+		 *		The element to update
+		 */
+		theme : function(template, variables, element) {
+			var output, method;
+			// normalize inputs
+			variables = !variables ? {} : variables;
+			// run the preprocess method if provided
+			if(typeof this['templatePreprocess_'+ template] == 'function') {
+				this['templatePreprocess_'+ template](variables);
+			}
+			// make the update type configurable from the variables array and preprocess function
+			method = variables.updateMethod ? variables.updateMethod : 'append';
+
+      if(element) {
+        try {
+          var templateBuffer = this.templates[template](variables);
+          // update the target element with content back from the template function
+          element[method](templateBuffer);
+          // rescan the dom for any new triggers
+          this.scan(element);
+          // trigger the custom event
+          $('body').trigger('blueprintThemeInserted', [template]);
+          // return the template
+          return templateBuffer;
         }
-
-        // run the theme function
-        var buffer = this.templates[template](variables);
-
-        // run the preprocess method if provided
-        if(typeof options.postprocess == 'function') {
-          options.postprocess(buffer);
+        catch(e) {
+          console.log(e);
         }
+			}
+			else {
+			  return this.templates[template](variables);
+			}
 
-        // if no element is specified, just return the buffer
-        if(!element.length) return buffer;
+		},
+		scan : function (element) {
+			var elements = !element ? $('.blueprint-js') : $(element.find('.blueprint-js'));
+			// loop through the DOM and find init elements
+			elements.each(function(index, element) {
+				element = $(element);
+				var id = element.attr('id');
+				try {
+					controllers[id] = new Blueprint(window[id + 'ViewController'], element);
+				}
+				catch(e) {
+					console.log(e);
+				}
+			});
+		}
 
-        // update the target element with content back from the template function
-        element[options.insertMethod](buffer);
+	});
 
-        // run the preprocess method if provided
-        if(typeof options.afterInsert == 'function') {
-          options.afterInsert(element);
-        }
+	$(document).ready(Blueprint.scan);
 
-        // return the buffer
-        return buffer;
-
-      }
-    }
-
-  };
-
-  return Blueprint();
-
-})( typeof Zepto == 'object' ? Zepto : $, window, document );
+})( jQuery );
